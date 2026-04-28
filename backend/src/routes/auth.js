586169ee923c172960ead18b2dd2ce65148fd3e7
godzaroky <gon24170@uvg.edu.gt>
2026-04-28 17:26:17 -0600

    fix: correxion de bcrypt

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router  = express.Router();
 const pool    = require('../db');
-const bcrypt  = require('bcryptjs');
+const bcrypt  = require('bcrypt');
 
 // POST /api/auth/login
 router.post('/login', async (req, res) => {
